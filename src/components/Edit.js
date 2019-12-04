439dbeca1d9a809e2ba9af854385cd91efa04d1c
mohit_jc <mkjcsoftwaresolution@gmail.com>
2019-12-04 23:01:42 +0530

    updated

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,85 +1,46 @@
-import React from 'react';
-import { tsConstructorType } from '@babel/types';
-const Edit = () => { 
-    
-  const [name,setName]=React.useState("sanjeet")
-  //  const handleSubmit =(e) => {
-   
-  //     e.preventDefault();
-  //     const id = this.props.match.params.id
-  //     const url = `http://dummy.restapiexample.com/api/v1/update/${id}`
+import React, {useState, useEffect} from 'react';
 
-  //     const data = {
-  //         name: this.state.name,
-  //         salary: this.state.salary,
-  //         age: this.state.age
-    
-  //       };
+const Edit = ({id}) => { 
+const [name, setName] = useState("")
+const [age, setAge] = useState("")
+const [salary, setSalary] = useState("")
 
-  //     fetch(url, {
-  //         method: 'PUT',
-  //         body: JSON.stringify(data),
-  //         headers: {
-  //             'Content-Type': 'application/json'
-  //         }
-  //     })
-  //         .then(response => response.json())
-  //         .then(data => {
-  //             console.log("UPDATED DATA", data)
-  //             if(data){
-  //                 this.props.history.push('/dashboard')
-  //                 alert("User Updated Successfully");
-  //             } else{
-  //                 alert("Error while Updating");
-
-  //             }
-  //           })
-  //         }
-
-//   useEffect(()=>{
-//     const id = this.props.match.params.id
-//     const url = `http://dummy.restapiexample.com/api/v1/employee/${id}`;
-
-//     fetch(url, {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log("single user data", data)
-//             this.setState({ name: data.employee_name, salary: data.employee_salary, age: data.employee_age, })
-//         });
-
-// }
-
-// return(
-//   <div>
-//     hello
-//   </div>
-//           // <div className="main-heading">
-//           // <div className="form-group">
-//           //   <label for="pwd">First Name:</label>
-//           //   <input type="text" className="form-control"  id="pwd" placeholder="first_name" name="fn" />
-     
-//           //   <label for="pwd">Age:</label>
-//           //   <input type="password" className="form-control" id="pwd" placeholder="last_name" name="age" />
-          
-//           //   <label for="sal">Salery:</label>
-//           //   <input type="taxt" className="form-control"  id="email" placeholder="Enter Salery" name="Sal" />
-//           // </div>
-//           // <button type="submit" className="btn-set btn-default">Update</button>
-//           // </div>
-//     )
-
-return(
-  <div>
-    <h1>{name}</h1>
-    <button onClick={() => setName("ajeet")}>Submit</button>
-  </div>
-)
+const handlesubmit = (e) =>{
+  e.preventDefault()
+    const url = `http://dummy.restapiexample.com/api/v1/update/${id}`;
+const data ={
+  name: name,
+  salary: salary,
+  age: age
 }
+    fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+            
+        }
+    })
+        .then(response => response.json())
+        .then(res => {
+            console.log("Edit Response", res)
+        })
+      }
 
+          return(
+          <form className="main-heading" onSubmit={handlesubmit}>
+           id:  {id}
+          <div className="form-group">
+            <label for="pwd">Name:</label>
+            <input type="text" className="form-control" value={name} onChange={(e)=>setName(e.target.value)} id="pwd" placeholder="name" name="fn" />
+            <label for="pwd">Age:</label>
+            <input type="number" className="form-control" value={age} onChange={(e)=>setAge(e.target.value)} id="pwd" placeholder="age" name="age" />
+            <label for="sal">Salery:</label>
+            <input type="number" className="form-control"  value={salary} onChange={(e)=>setSalary(e.target.value)} id="email" placeholder="Enter Salery" name="Sal" />
+          </div>
+          <input type="submit" className="btn-set btn-default" value="Update"/>
+          </form>
+    )
+}
   export default Edit;
   
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Edit from '../components/Edit';
 const Home = () => {
     const [employees, setEmployees] = useState(null)
     const [showForm, setShowForm] = useState(false)
+    const [id, setId] = useState(null)
     useEffect(()=>{
         getEmployees();
 
@@ -16,30 +17,30 @@ const Home = () => {
             setEmployees(res)
         })
     }
-  const handleShowForm = () => {
+  const handleShowForm = (i) => {
     setShowForm(!showForm)
+    setId(i)
   }
 console.log("employees",employees)
     return (
         <div className="container">
-            <div className="row">
+         <div className="row">
         <div className="col-md-8">
             <table>
             {employees && employees.map((item)=>  
-            
             <tr>
             <td>{item.employee_name}</td>
             <td>{item.employee_salary}</td>
             <td>{item.employee_age}</td>
-            <button className="btns" onClick={handleShowForm}>Update</button>
+            <button className="btns" onClick={()=>handleShowForm(item.id)}>Update</button>
             <button className="btnD" >Delete</button>
            </tr> 
-            )  }
-               
+            ) 
+            } 
             </table>
             </div>
             <div className="col-md-4">
-            {showForm === true ? <Edit />: null}
+            {showForm === true ? <Edit id={id} />: null}
             </div>
             </div>
         </div>
